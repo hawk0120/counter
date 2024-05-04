dbb33ae929da553c86679423f3cbfc5b2b3154d6
Brady Hawkins <brady93hawkins@gmail.com>
2024-05-04 13:45:30 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View} from "react-native";
+
+export default function App() {
+  const [counter, setCounter] = useState(0);
+  const onPressIncrement = () => {
+    setCounter(counter + 1);
+  };
+
+  const onPressDecrement = () => {
+    setCounter(counter - 1);
+  };
+  return (
+    <View style={styles.container}>
+      <Text>{counter}</Text>
+      <View style={styles.row}>
+        <Button
+          onPress={() => onPressIncrement()}
+          title="Increment"
+          accessibilityLabel="Add one to the counter"
+        />
+        <Button
+          onPress={() => onPressDecrement()}
+          title="Decrement"
+          accessibilityLabel="Add one to the counter"
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  row: {
+				flexDirection: "row",
+				justifyContent: "space-evenly",
+				width:  300
+	},
+
+});
